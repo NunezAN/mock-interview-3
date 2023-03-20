e6233c6788b4821aecfef74331df787428f7f9a0
NunezAN <alex101nunez@gmail.com>
2023-03-20 11:28:09 -0500

    set up states

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import { states } from "./States";
 
 function App() {
-  const [results, setResults] = useState({});
-  useEffect(() => {
-    const fetchResults = async () => {
-      const results = await axios.get("https://www.boredapi.com/api/activity");
-
-      setResults(results.data);
-      // console.log(results.data);
-    };
-    fetchResults();
-  }, []);
   return (
     <div className="bg-black w-screen h-screen text-white flex flex-col">
-      <span className="text-white text-4xl text-center mt-12">
-        {results.activity}
-      </span>
+      <select className="bg-inherit w-fit mx-auto text-2xl p-12">
+        <option>--select state--</option>
+        {states.map((elem, i) => (
+          <option key={i}>{elem.name}</option>
+        ))}
+      </select>
+      <div className="p-12 flex flex-wrap items-center justify-between">
+        {states.map((elem, index) => (
+          <div key={index} className="w-48 p-2">
+            <input type="checkbox" />
+            <span>{elem.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
