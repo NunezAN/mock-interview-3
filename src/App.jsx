0a62f74af65eab2b0d31a8139810dd7480e6622c
NunezAN <alex101nunez@gmail.com>
2023-03-21 22:51:47 -0500

    completed coding challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { states } from "./States";
+import { useState } from "react";
+import { statesObj } from "./States";
 
 function App() {
+  const [states, setStates] = useState(statesObj);
+
+  const handleStateSelected = (stateSelected) => {
+    console.log(stateSelected);
+    setStates((current) =>
+      current.map((state) => {
+        if (state.name === stateSelected) {
+          return { ...state, checked: !state.checked };
+        } else {
+          return state;
+        }
+      })
+    );
+  };
   return (
     <div className="bg-black w-screen h-screen text-white flex flex-col">
-      <select className="bg-inherit w-fit mx-auto text-2xl p-12">
+      <select
+        className="bg-inherit w-fit mx-auto text-2xl p-12"
+        onChange={(e) => handleStateSelected(e.target.value)}
+      >
         <option>--select state--</option>
         {states.map((elem, i) => (
           <option key={i}>{elem.name}</option>
@@ -12,7 +30,11 @@ function App() {
       <div className="p-12 flex flex-wrap items-center justify-between">
         {states.map((elem, index) => (
           <div key={index} className="w-48 p-2">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={elem.checked}
+              onChange={() => handleStateSelected(elem.name)}
+            />
             <span>{elem.name}</span>
           </div>
         ))}
